1c5844689a2ba3e5977b426dd02271c8e3c9ff05
Dalchandra Sharma <39084636+dalchandra@users.noreply.github.com>
2022-07-15 18:51:11 +0530

    Update lamp.sh

--- install/lamp.sh
+++ install/lamp.sh
@@ -4,6 +4,9 @@ sudo apt-get upgrade
 sudo apt-get install php -y
 sudo apt-get install mysql-server -y
 sudo mysql
+CREATE USER 'test_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'EXAMPLE_PASSWORD';
+GRANT ALL PRIVILEGES ON test_database.* TO 'test_user'@'localhost';
+FLUSH PRIVILEGES;
 exit
 sudo apt-get install phpmyadmin -y
 echo 'Include /etc/phpmyadmin/apache.conf' /etc/apache2/apache2.conf
